b7149c92a05e03aa2e878f699018d82aa6b86f11
mateusz7988 <108484575+mateusz7988@users.noreply.github.com>
2022-11-03 21:39:39 +0100

    Add files via upload

---
+++ Onos1.bat
@@ -0,0 +1,3 @@
+curl --user onos:rocks -X GET "http://mininet:8181/onos/v1/devices" -H "Accept: application/json"
+
+pause
---
+++ Onos2.bat
@@ -0,0 +1,3 @@
+curl --user onos:rocks -X POST "http://mininet:8181/onos/v1/flows/of:0000000000000001" -d @Onos2.json -H "Content-Type: application/json" -H "Accept: application/json"
+
+pause
